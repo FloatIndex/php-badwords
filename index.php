2fb37adf1bea75c2a92b3871387ec1778952a930
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-03-11 15:25:36 +0700

    empty get control

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 $paragraph = "Tyger tyger burning bright in the forests of the night: what immortal hand or eye could frame thy fearful symmetry?";
 $paragraphLength = strlen($paragraph);
 
-$censoredWord = $_GET['censored'];
+if(!empty($_GET['censored'])) {
+    $censoredWord = $_GET['censored'];
+    $censoredParagraph = str_ireplace($censoredWord, '***', $paragraph);
+    $censoredParagraphLength = strlen($censoredParagraph);
+    $check = true;
+} else {
+    $check = false;
+}
 
-$censoredParagraph = str_ireplace($censoredWord, '***', $paragraph);
-$censoredParagraphLength = strlen($censoredParagraph);
+
 ?>
 
 
@@ -19,14 +25,18 @@ $censoredParagraphLength = strlen($censoredParagraph);
     <title>Document</title>
 </head>
 <body>
-    <h1>Paragrafo originale:</h1>
+    <h1>Paragrafo:</h1>
     <p><?php echo $paragraph ?></p>
-    <h4>Il paragrafo originale è composto da <?php echo $paragraphLength ?> caratteri</h4>
+    <h4>Il paragrafo è composto da <?php echo $paragraphLength ?> caratteri</h4>
+    
+    <?php if($check) {?>
+        <br><br>
 
-    <br><br>
-
-    <h1>Paragrafo censurato:</h1>
-    <p><?php echo $censoredParagraph ?></p>
-    <h4>Il paragrafo censurato è composto da <?php echo $censoredParagraphLength ?> caratteri</h4>
+        <h1>Paragrafo censurato:</h1>
+        <p><?php echo $censoredParagraph ?></p>
+        <h4>Il paragrafo censurato è composto da <?php echo $censoredParagraphLength ?> caratteri</h4>
+    <?php } ?>
+    
+    
 </body>
 </html>
